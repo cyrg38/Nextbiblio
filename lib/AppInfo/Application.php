d270962586b170c6280cab5f0bae10584c63d314
cyrg38 <cyrg38@users.noreply.github.com>
2021-06-13 20:10:31 +0200

    Update Application.php

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace OCA\NotesTutorial\AppInfo;
+namespace OCA\Nextbiblio\AppInfo;
 
 use OCP\AppFramework\App;
 
 class Application extends App {
-	public const APP_ID = 'notestutorial';
+	public const APP_ID = 'nextbiblio';
 
 	public function __construct() {
 		parent::__construct(self::APP_ID);
