1a292e040bb14263da7cd9c77e7a509cebc6996d
cyrg38 <cyrg38@users.noreply.github.com>
2021-06-13 20:14:32 +0200

    Update Errors.php

--- lib/Controller/Errors.php
+++ lib/Controller/Errors.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace OCA\NotesTutorial\Controller;
+namespace OCA\Nextbiblio\Controller;
 
 use Closure;
 
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataResponse;
 
-use OCA\NotesTutorial\Service\NoteNotFound;
+use OCA\Nextbiblio\Service\NoteNotFound;
 
 trait Errors {
 	protected function handleNotFound(Closure $callback): DataResponse {
