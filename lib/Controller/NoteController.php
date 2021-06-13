c535f9555c353879f55f5586c6966519912c83f8
cyrg38 <cyrg38@users.noreply.github.com>
2021-06-13 20:12:43 +0200

    Update NoteController.php

--- lib/Controller/NoteController.php
+++ lib/Controller/NoteController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OCA\NotesTutorial\Controller;
+namespace OCA\Nextbiblio\Controller;
 
 use OCA\NotesTutorial\AppInfo\Application;
 use OCA\NotesTutorial\Service\NoteService;
@@ -41,23 +41,74 @@ class NoteController extends Controller {
 		});
 	}
 
-	/**
-	 * @NoAdminRequired
-	 */
-	public function create(string $title, string $content): DataResponse {
-		return new DataResponse($this->service->create($title, $content,
-			$this->userId));
-	}
+    /**
+     * @NoAdminRequired
+     *
+     * @param string $title
+     * @param string $_emplacement
+     * @param string $_isbn
+     * @param string $_lu
+     * @param string $_period
+     * @param string $uuid
+     * @param string $publisher
+     * @param string $isbn
+     * @param string $identifiers
+     * @param string $authors
+     * @param string $timestamp
+     * @param string $pubdate
+     * @param string $tags
+     * @param string $languages
+     * @param string $cover
+     * @param string $library_name
+     * @param text $comments
+     */
+    public function create(string $title, string $_emplacement, string $_isbn, string $_lu, 
+                           string $_period, string $uuid, string $publisher, string $isbn, string $identifiers,
+                           string $authors, string $timestamp, string $pubdate, string $tags, string $languages,
+                           string $cover, string $library_name, text $comments,
+                           string $userId) {
+        return $this->service->create( $title,  $_emplacement,  $_isbn,  $_lu, 
+                            $_period,  $uuid,  $publisher,  $isbn,  $identifiers,
+                            $authors,  $timestamp,  $pubdate,  $tags,  $languages,
+                            $cover,  $library_name, $comments,
+                            $this->userId);
+    }
 
-	/**
-	 * @NoAdminRequired
-	 */
-	public function update(int $id, string $title,
-						   string $content): DataResponse {
-		return $this->handleNotFound(function () use ($id, $title, $content) {
-			return $this->service->update($id, $title, $content, $this->userId);
-		});
-	}
+    /**
+     * @NoAdminRequired
+     *
+     * @param int $id
+     * @param string $title
+     * @param string $_emplacement
+     * @param string $_isbn
+     * @param string $_lu
+     * @param string $_period
+     * @param string $uuid
+     * @param string $publisher
+     * @param string $isbn
+     * @param string $identifiers
+     * @param string $authors
+     * @param string $timestamp
+     * @param string $pubdate
+     * @param string $tags
+     * @param string $languages
+     * @param string $cover
+     * @param string $library_name
+     * @param text $comments
+     */
+    public function update(int $id, string $title, string $_emplacement, string $_isbn, string $_lu, 
+                           string $_period, string $uuid, string $publisher, string $isbn, string $identifiers,
+                           string $authors, string $timestamp, string $pubdate, string $tags, string $languages,
+                           string $cover, string $library_name, text $comments) {
+        return $this->handleNotFound(function () use ($id, $title,  $_emplacement,  $_isbn,  $_lu, 
+                            $_period,  $uuid,  $publisher,  $isbn,  $identifiers,
+                            $authors,  $timestamp,  $pubdate,  $tags,  $languages,
+                            $cover,  $library_name, $comments)
+            return $this->service->update($id, $title,  $_emplacement,  $_isbn,  $_lu, 
+                            $_period,  $uuid,  $publisher,  $isbn,  $identifiers,
+                            $authors,  $timestamp,  $pubdate,  $tags,  $languages,
+                            $cover,  $library_name, $comments, $this->userId);
+
 
 	/**
 	 * @NoAdminRequired
